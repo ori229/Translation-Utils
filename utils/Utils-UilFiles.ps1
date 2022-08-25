cebed8cec4297656eb4a5ee206ba18d47e5f4598
YehuditAdler <dittyadler@gmail.com>
2022-08-25 11:42:22 +0300

    trim new line

--- utils/Utils-UilFiles.ps1
+++ utils/Utils-UilFiles.ps1
@@ -124,7 +124,7 @@ function createNewUilFile($filename) {
                 $langCodeForCol = $langCodes[$j]
                 $hashKey = $codeTableName + $DEL + $code + $DEL + $langCodeForCol
                 if ($tableCodeAndLangToText_Excel.ContainsKey($hashKey) -and $tableCodeAndLangToText_Excel[$hashKey].Trim() -ne $existingTranslation.Trim() ) {
-                    $newTranslation = $tableCodeAndLangToText_Excel[$hashKey].trim()
+                    $newTranslation = $tableCodeAndLangToText_Excel[$hashKey].Trim([char]0x000A, [char]0x0020, [char]0x200B)
                     log " We have new translation for this lang - $hashKey : $newTranslation"
                     $tempNum = $updatedLineArray.Add($newTranslation)
                 } else {
