991c35e6219c3946983e91d03c1207f56bc9dc12
YehuditAdler <dittyadler@gmail.com>
2022-11-15 09:54:45 +0200

    PreviousBranchName  logs

--- utils/Utils-General.ps1
+++ utils/Utils-General.ps1
@@ -71,15 +71,18 @@ function getPreviousBranchName() {
     [string]$content = (Invoke-WebRequest -Uri $baseurl  -Headers $Headers -UseBasicParsing).Content 
     $html.IHTMLDocument2_write($content);
     $branches = $html.all.tags("li") | % InnerText
-
+	log "branches from url $branches ..."
     $PreviousBranchName = "September2020-PROD/"
     foreach ($date in $branches) {
         if($date -match "-PROD/"){
+			log "date is $date ..."
             if( [datetime]::parseexact(("01" + $PreviousBranchName -replace "-PROD/","").Trim(), 'ddMMMMyyyy',$null) -lt [datetime]::parseexact(("01" + $date -replace "-PROD/","").Trim(), 'ddMMMMyyyy',$null)){
                 $PreviousBranchName = $date
+				log "setting PreviousBranchName to $PreviousBranchName ..."
             }
          }
     }
+	log "after foreach PreviousBranchName is $PreviousBranchName"
     return ($PreviousBranchName -replace "-PROD/","").Trim()
 }
 
